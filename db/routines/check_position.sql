b0c27900a57a008e58b37f68ae8ad4ace013507d
Georgi D. Sotirov <gdsotirov@dir.bg>
2018-06-16 20:22:32 +0300

    Add DNS (Did Not Start) as possible value

--- db/routines/check_position.sql
+++ db/routines/check_position.sql
@@ -3,9 +3,16 @@ DELIMITER //
 CREATE FUNCTION check_position(pos VARCHAR(3))
 RETURNS BOOL NO SQL
 BEGIN
+  /* Either a number */
   IF pos RLIKE '^[1-9][0-9]*$' THEN
     RETURN TRUE;
-  ELSEIF pos IN ('NC', 'DNF', 'DSQ') THEN
+  /* Ore one of these values
+   * NC  - Not classified
+   * DNF - Did Not Finish
+   * DNS - Did Not Start
+   * DSQ - Disqualified
+   */
+  ELSEIF pos IN ('NC', 'DNF', 'DNS', 'DSQ') THEN
     RETURN TRUE;
   ELSE
     RETURN FALSE;
