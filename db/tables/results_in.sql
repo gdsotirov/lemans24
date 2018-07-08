bebc4f9b29c6f16754928ea785ac4f7d7ce2436e
Georgi D. Sotirov <gdsotirov@dir.bg>
2018-07-08 23:31:42 +0300

    Add results until 1992, fix some wrong data that cannot be imported

--- db/tables/results_in.sql
+++ db/tables/results_in.sql
@@ -1,11 +1,11 @@
 CREATE TABLE results_in (
   race_yr       INT           NOT NULL  COMMENT 'Race year',
   pos           VARCHAR(3)    NOT NULL  COMMENT 'Position - either number or NC (not classified), DNF (do not finish) or DSQ (disqualified)',
-  car_class     VARCHAR(8)    NOT NULL  COMMENT 'Car class (litres or named)',
+  car_class     VARCHAR(12)   NOT NULL  COMMENT 'Car class (litres or named)',
   car_nbr       INT           NOT NULL  COMMENT 'Car number',
   team_cntry    VARCHAR(16)   NOT NULL  COMMENT 'Team country (ISO 3166 3 characters code)',
   team_name     VARCHAR(128)  NOT NULL  COMMENT 'Team name',
-  drivers_cntry VARCHAR(16)   NOT NULL  COMMENT 'Drivers countries (ISO 3166 3 characters code) separated',
+  drivers_cntry VARCHAR(20)   NOT NULL  COMMENT 'Drivers countries (ISO 3166 3 characters code) separated',
   drivers_name  VARCHAR(256)  NOT NULL  COMMENT 'Drivers names',
   chassis       VARCHAR(64)   NOT NULL  COMMENT 'Chassis name',
   engine        VARCHAR(64)   NOT NULL  COMMENT 'Engine name',
