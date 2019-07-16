349ff36204faa19a6e9ee3f3ce76708ced6bac74
Georgi D. Sotirov <gdsotirov@gmail.com>
2019-07-16 19:21:40 +0300

    Add full name column in drivers table, extend unique index over nickname, title and sex and reorder columns, add full-text index over title, first and last name, nickname and suffix

--- db/tables/drivers.sql
+++ db/tables/drivers.sql
@@ -5,14 +5,35 @@ CREATE TABLE IF NOT EXISTS drivers (
   lname     VARCHAR(32)     NOT NULL,
   nickname  VARCHAR(32)     NULL,
   nm_suffix VARCHAR(4)      NULL COMMENT 'Name suffix (e.g. Jr., Sr., III)',
+  full_name VARCHAR(128)    GENERATED ALWAYS AS (
+                              CONCAT(CASE WHEN title NOT LIKE 'Baron %' AND
+                                               title NOT LIKE 'Earl %'  AND
+                                               title NOT LIKE 'Lord %'
+                                          THEN CONCAT(title, ' ') ELSE ''  END, /* title in front */
+                                     CASE WHEN fname     IS NULL THEN ''
+                                          ELSE CONCAT(fname, ' ')          END, /* first name if known */
+                                     CASE WHEN nickname  IS NULL THEN ''
+                                          ELSE CONCAT('"', nickname, '" ') END, /* nickname if any */
+                                     lname,                                     /* last name */
+                                     CASE WHEN title LIKE 'Baron %' OR
+                                               title LIKE 'Earl %'  OR
+                                               title LIKE 'Lord %'
+                                          THEN CONCAT(', ', title) ELSE '' END, /* title after name */
+                                     CASE WHEN nm_suffix IS NULL THEN ''
+                                          ELSE CONCAT(' ', nm_suffix)      END  /* name suffix if any */
+                                    )
+                            )
+                            VIRTUAL
+                            COMMENT 'Driver full name including title, names, nickname and suffix',
   sex       ENUM('M', 'F')  NULL,
   born      DATE            NULL,
   country   CHAR(4)         NOT NULL,
 
   PRIMARY KEY (id),
 
-  UNIQUE INDEX idx_driver_unq (fname ASC, lname ASC, country ASC, nm_suffix ASC)
-  INDEX idx_driver_sex (sex ASC)
+  UNIQUE INDEX idx_driver_unq (lname ASC, sex ASC, country ASC, fname ASC, nickname ASC, title ASC, nm_suffix ASC),
+  INDEX idx_driver_sex (sex ASC),
+  FULLTEXT INDEX idx_driver_full (title, fname, lname, nickname, nm_suffix)
 )
 ENGINE = InnoDB
 COMMENT = 'Drivers register';
