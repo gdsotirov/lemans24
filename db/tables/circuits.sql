cc8b73bf8ff47cfe00fc793cb406ea4392f7a31f
Georgi D. Sotirov <gdsotirov@gmail.com>
2020-09-14 19:15:57 +0300

    Round also column circuits.length_mi

--- db/tables/circuits.sql
+++ db/tables/circuits.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS circuits (
   name        VARCHAR(45)   NOT NULL,
   since       DATE          NOT NULL,
   length_km   DECIMAL(5,3)  NULL,
-  length_mi   DECIMAL(5,3)  GENERATED ALWAYS AS (length_km*0.621371192) VIRTUAL,
+  length_mi   DECIMAL(5,3)  GENERATED ALWAYS AS (ROUND(length_km * 0.621371192, 3)) VIRTUAL,
   layout      LONGBLOB      NULL,
   changes     VARCHAR(128)  NULL,
 
