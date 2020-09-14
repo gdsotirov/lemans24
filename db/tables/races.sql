e4332e99487a0983e4cbb807e44722520b80bad9
Georgi D. Sotirov <gdsotirov@gmail.com>
2020-09-14 19:05:22 +0300

    Round results for virtual columns
    Round calculated result for virtual columns distance_mi, avg_speed_kmh
    and avg_speed_mph to avoid 'Data truncated for column' warnings on
    update of table races. Rounding is done to column's scale.

--- db/tables/races.sql
+++ db/tables/races.sql
@@ -6,10 +6,10 @@ CREATE TABLE IF NOT EXISTS races (
   cancelled           TINYINT       NOT NULL,
   cancellation_reason VARCHAR(128)  NULL,
   distance_km         DECIMAL(10,3) NULL,
-  distance_mi         DECIMAL(10,3) GENERATED ALWAYS AS (distance_km*0.621371192) VIRTUAL,
+  distance_mi         DECIMAL(10,3) GENERATED ALWAYS AS (ROUND(distance_km   * 0.621371192, 3)) VIRTUAL,
   laps                DECIMAL(7,3)  NOT NULL COMMENT 'Number of laps',
-  avg_speed_kmh       DECIMAL(5,2)  GENERATED ALWAYS AS (distance_km/24),
-  avg_speed_mph       DECIMAL(5,2)  GENERATED ALWAYS AS (avg_speed_kmh*0.621371192) VIRTUAL,
+  avg_speed_kmh       DECIMAL(5,2)  GENERATED ALWAYS AS (ROUND(distance_km   / 24         , 2)) VIRTUAL,
+  avg_speed_mph       DECIMAL(5,2)  GENERATED ALWAYS AS (ROUND(avg_speed_kmh * 0.621371192, 2)) VIRTUAL,
 
   PRIMARY KEY (id),
 
