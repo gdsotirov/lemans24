af0c1ac8f8301267cd99d8279fd8b1d2514d051f
Georgi D. Sotirov <gdsotirov@gmail.com>
2022-06-25 13:52:19 +0300

    Turn some empty fields to NULL
    Turn empty fields to NULL when loading data for columns car_tyres,
    racing_time and reason, which are not always populated.
    
    See https://dev.mysql.com/doc/refman/8.0/en/load-data.html#load-data-column-assignments

--- db/data/ex_load_data.sql
+++ db/data/ex_load_data.sql
@@ -1,12 +1,17 @@
 LOAD DATA INFILE '/var/mysql/files/results_in.csv'
   INTO TABLE results_in
   FIELDS TERMINATED BY ';'
-  OPTIONALLY ENCLOSED BY '"'
+    OPTIONALLY ENCLOSED BY '"'
+  LINES TERMINATED BY '\r\n'
   IGNORE 1 LINES
   (race_yr, pos, car_class, car_nbr,
    team_cntry, team_name,
    drivers_cntry, drivers_name,
-   car_chassis, car_engine, car_tyres,
+   car_chassis, car_engine, @car_tyres,
    laps, distance,
-   racing_time, reason);
+   @racing_time, @reason)
+  SET
+    car_tyres   = CASE @car_tyres   WHEN '' THEN NULL ELSE @car_tyres   END,
+    racing_time = CASE @racing_time WHEN '' THEN NULL ELSE @racing_time END,
+    reason      = CASE @reason      WHEN '' THEN NULL ELSE @reason      END;
 
