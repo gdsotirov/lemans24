42b83b8e40d8d0a1706e3ce6f671f0c0a6512245
Georgi D. Sotirov <gdsotirov@gmail.com>
2024-06-16 19:16:07 +0300

    Split Not classified from Disqualified

--- db/views/RacesByCarsFinished.sql
+++ db/views/RacesByCarsFinished.sql
@@ -4,8 +4,9 @@ SELECT race_id                                                                  
        SUM(CASE WHEN pos NOT REGEXP '^(DNA|DNP|DNQ|DNF|DNS|RES)$' THEN 1 ELSE 0 END) AS Finished,
        SUM(CASE WHEN pos NOT REGEXP '^(DNA|DNP|DNQ|DNF|DNS|RES)$' THEN 1 ELSE 0 END) * 100 /
        SUM(CASE WHEN pos NOT REGEXP '^(DNA|DNP|DNQ|DNS|RES)$'     THEN 1 ELSE 0 END) AS FinishedPercent,
-       SUM(CASE WHEN pos IN ('DSQ', 'NC')                         THEN 1 ELSE 0 END) AS Disqualified,
-       SUM(CASE WHEN pos IN ('DNF')                               THEN 1 ELSE 0 END) AS Retired
+       SUM(CASE WHEN pos = 'DSQ'                                  THEN 1 ELSE 0 END) AS Disqualified,
+       SUM(CASE WHEN pos = 'NC'                                   THEN 1 ELSE 0 END) AS Not_classified,
+       SUM(CASE WHEN pos = 'DNF'                                  THEN 1 ELSE 0 END) AS Retired
   FROM results RES
  WHERE race_id != 1936 /* cancelled */
  GROUP BY race_id
