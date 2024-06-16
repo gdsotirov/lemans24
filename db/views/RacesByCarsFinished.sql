23bf41d9897678bf840ae7f5e4a499e357a625d6
Georgi D. Sotirov <gdsotirov@gmail.com>
2024-06-16 04:07:25 +0300

    Calculate also percent finished cars

--- db/views/RacesByCarsFinished.sql
+++ db/views/RacesByCarsFinished.sql
@@ -2,6 +2,8 @@ CREATE OR REPLACE VIEW RacesByCarsFinished AS
 SELECT race_id                                                                       AS `Year`,
        SUM(CASE WHEN pos NOT REGEXP '^(DNA|DNP|DNQ|DNS|RES)$'     THEN 1 ELSE 0 END) AS Started,
        SUM(CASE WHEN pos NOT REGEXP '^(DNA|DNP|DNQ|DNF|DNS|RES)$' THEN 1 ELSE 0 END) AS Finished,
+       SUM(CASE WHEN pos NOT REGEXP '^(DNA|DNP|DNQ|DNF|DNS|RES)$' THEN 1 ELSE 0 END) * 100 /
+       SUM(CASE WHEN pos NOT REGEXP '^(DNA|DNP|DNQ|DNS|RES)$'     THEN 1 ELSE 0 END) AS FinishedPercent,
        SUM(CASE WHEN pos IN ('DSQ', 'NC')                         THEN 1 ELSE 0 END) AS Disqualified,
        SUM(CASE WHEN pos IN ('DNF')                               THEN 1 ELSE 0 END) AS Retired
   FROM results RES
